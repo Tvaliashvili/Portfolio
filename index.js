000d24367c28a678997264dba5e1a606e263c19a
Akalifiedman <sandrotvaliashvili10@gmail.com>
2025-02-03 06:31:32 +0400

    implemented navbar menu collapsible for small devices

---
+++ index.js
@@ -0,0 +1,10 @@
+let collapsible = document.getElementById(`navMenu`)
+
+function showMenuNavBar() {
+    if(collapsible.classList.contains(`collapsible-container-hide`)) {
+        collapsible.classList.replace(`collapsible-container-hide`, `collapsible-container-show`)
+    }
+    else {
+        collapsible.classList.add(`collapsible-container-hide`)
+    }
+}
